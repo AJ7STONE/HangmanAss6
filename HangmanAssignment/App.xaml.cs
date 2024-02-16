8431a6d6e59f58cdec00c553985de7ec3909dd47
AJ7STONE <5100168@mypillar7uwc.ac.za>
2024-02-16 15:46:23 +0200

    Initial Commit

---
+++ HangmanAssignment/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace HangmanAssignment
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
